3b9b5198932cd342d6fecec70ac52c737cc89f81
Bhavnasahu98 <sahuybhavna2941@gmail.com>
2024-10-10 20:53:19 +0530

    Optimized knapsack algorithm with reduced space complexity

--- C++/Knapsack.cpp
+++ C++/Knapsack.cpp
@@ -1,37 +1,30 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
-
-int knapsack(int W, const vector<int>& weights, const vector<int>& values) {
+int knapsack(int W, const std::vector<int>& weights, const std::vector<int>& values) {
     int n = weights.size();
-    vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
+    std::vector<int> dp(W + 1, 0);
 
-    for (int i = 1; i <= n; ++i) {
-        for (int w = 0; w <= W; ++w) {
-            if (weights[i - 1] <= w) {
-                dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - weights[i - 1]] + values[i - 1]);
-            } else {
-                dp[i][w] = dp[i - 1][w];
-            }
+    for (int i = 0; i < n; ++i) {
+        for (int w = W; w >= weights[i]; --w) {
+            dp[w] = std::max(dp[w], dp[w - weights[i]] + values[i]);
         }
     }
 
-    return dp[n][W];
+    return dp[W];
 }
 
 int main() {
     int W, n;
-    cin >> W >> n;
-    vector<int> weights(n);
-    vector<int> values(n);
+    std::cin >> W >> n;
+    std::vector<int> weights(n), values(n);
 
     for (int i = 0; i < n; ++i) {
-        cin >> weights[i] >> values[i];
+        std::cin >> weights[i] >> values[i];
     }
 
-    int result = knapsack(W, weights, values);
-    cout << result << endl;
+    std::cout << knapsack(W, weights, values) << std::endl;
 
     return 0;
 }
+
